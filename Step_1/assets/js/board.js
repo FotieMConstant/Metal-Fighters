50e3b42679e018a7d236fc7d7a2dece4346b8b18
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2020-03-25 22:49:31 +0100

    Created the elements and characters
    - Created a function that returns random number which will be used to generate random X and Y coordinates for where elements will be placed.
    - Practiced movements of elements on the map with coordinates. that works fine.
    - Elements can now be placed randomely on the map #ATestPhaseForNow!

--- Step_1/assets/js/board.js
+++ Step_1/assets/js/board.js
@@ -2,7 +2,7 @@
 
 // Board class
 
-class board {
+class Board {
     constructor(grid_nb){
         this.nb_grids = grid_nb;
     }
@@ -10,25 +10,60 @@ class board {
     createGrid(){
         for (var rows = 0; rows < this.nb_grids; rows++) {
             for (var columns = 0; columns < this.nb_grids; columns++) {
-                $("#container").append("<div class='grid'> X= "+columns+",Y="+rows+"</div>");
-                 
-                
+                $("#container").append('<div id="grid_'+columns+'_'+rows+'" class="grid"></div>');
+                 /*
                  console.log("Columns = "+columns);
                  console.log("Rows = "+rows);
+                 */
                  
             };
            
         };
-        $(".grid").width(660/this.nb_grids);
-        $(".grid").height(660/this.nb_grids);
+        $(".grid").width(760/this.nb_grids);
+        $(".grid").height(760/this.nb_grids);
     }
+
+    randomRAC() {
+        let genNumber = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+        return genNumber;
+    }
+
+    //to place an alement on a spacified coordinate
+    placeElement(row, col, element){
+    $("#grid_"+row+"_"+col+"").css('background-image', 'url(' + element + ')');
+
+    }
+
 }
 
 
 // create a 16x16 grid when the page loads
 $(document).ready(function() {
 
-    theBoard = new board(10);
+    theBoard = new Board(10);
     theBoard.createGrid();
+
+   let row = theBoard.randomRAC();
+   let column = theBoard.randomRAC();
+   console.log(row);
+   console.log(column);
+
+   theBoard.placeElement(row, column, 'assets/img/wall.png');
+
+   //Test randome display
+   let rowPlayer1 = theBoard.randomRAC();
+   let columnPlayer1 = theBoard.randomRAC();
+   
+   let rowPlayer2 = theBoard.randomRAC();
+   let columnPlayer2 = theBoard.randomRAC();
+   theBoard.placeElement(rowPlayer1, columnPlayer1, 'assets/img/player1.png');
+   theBoard.placeElement(rowPlayer2, columnPlayer2, 'assets/img/player2.png');
+
+
+
+    /*
+    let imageUrl = 'assets/img/wall.png';
+    $("#grid_4_5").css('background-image', 'url(' + imageUrl + ')');
+    */
 
 });
