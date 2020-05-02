d8220b9c7b41e5c22f5cda59b6aa93092f4eab61
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2020-05-03 00:38:19 +0100

    Fight is almost done
    Both players can fight now well and get the winner announced when one player hits 0, however still to work on the defend point deduction

--- Step_3 Fight/assets/js/board.js
+++ Step_3 Fight/assets/js/board.js
@@ -623,28 +623,72 @@ clearCell = () => {
 fight = () =>{
     //Event Listerners for the fight
     let playerName = this.currentTurn;
+    //Player's life
+    let Player1Life = 100;
+    let Player2Life = 100;
+    //Keeping track if the player has defended or not
+    let Player1Defend = false;
+    let Player2Defend = false;
+
 
     $('#Player1-attack').click(function() {
+        let currentPlayer = theBoard.playerStore.find((playerObj) => {return playerObj.name == playerName });
+       //In case the player has defended
+        if(Player2Defend){
+            Player2Life = Player2Life - (currentPlayer.attack / 2);
+            $('.Player2-life').text(Player2Life);
+        }
+        //In case the player hasn't defended
+        Player2Life = Player2Life - currentPlayer.attack;
+         $('.Player2-life').text(Player2Life);
+       
+         //When the opponent's player's life is less than or equal to 0 declare winner
+         if(Player2Life <= 0){
+             console.log("Player 1 wins");
+             $("#winner-avatar").append('<img class="player-player1" src="assets/img/player1-poster.png" alt="Player-1">')
+            $("#winner").text("Player 1 Wins");
+            window.location.href = "#gameover-modal"; //Calling for the gameover modal
+         }
         playerName = 'Player2'
         handleButtons(playerName);
     });
 
     $('#Player1-defend').click(function() {
+        Player1Defend = true; //Player 1 defending
         playerName = 'Player2'
         handleButtons(playerName);
     });
 
     $('#Player2-attack').click(function() {
+        let currentPlayer = theBoard.playerStore.find((playerObj) => {return playerObj.name == playerName });
+        //In case the player has defended
+        if(Player1Defend){
+            Player1Life = Player1Life - (currentPlayer.attack / 2);
+            $('.Player1-life').text(Player1Life);
+        }
+        //In case the player hasn't defended
+        Player1Life = Player1Life - currentPlayer.attack;
+         $('.Player1-life').text(Player1Life);
+
+         //When the opponent's player's life is less than or equal to 0 declare winner
+         if(Player1Life <= 0){
+            console.log("Player 2 wins");
+            $("#winner-avatar").append('<img class="player-poster2" src="assets/img/player2-poster.png" alt="Player-2">')
+            $("#winner").text("Player 2 Wins");
+            window.location.href = "#gameover-modal"; //Calling for the gameover modal
+        }
         playerName = 'Player1'
         handleButtons(playerName);
     });
 
     $('#Player2-defend').click(function() {
+        Player2Defend = true; //Player 2 defending
         playerName = 'Player1'
         handleButtons(playerName);
     });
+    //Calling for the styling function for the buttons
     handleButtons(playerName);
-        
+    
 }
 
 }//end of class
